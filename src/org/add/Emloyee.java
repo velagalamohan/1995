ca6f66e78897bee3964f84762c00fc00cddb83c8
velagalamohan <velagalamohan@gmail.com>
2021-12-25 20:56:17 +0530

    done

---
+++ src/org/add/Emloyee.java
@@ -0,0 +1,13 @@
+package org.add;
+
+public class Emloyee {
+	public static void companyName() {
+		
+		System.out.println("wipro");
+
+	}
+	public static void main(String[] args) {
+		companyName();
+	}
+
+}
